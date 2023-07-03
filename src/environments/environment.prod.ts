58cb16a71f4dc13442555898345fa9f9495eeae8
carterco <carterco@ats-inc.com>
2023-07-03 12:47:54 -0500

    added firebase connections

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  apiKey: '5500e335a9798fe6311bf02989382e48',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'https://image.tmdb.org/t/p',
 };
